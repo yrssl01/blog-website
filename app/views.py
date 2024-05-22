cde56d58a7810db5ce6a496790dfb11f49cf66d3
Yerassyl <erasik01@gmail.com>
2024-05-23 02:48:43 +0500

    view modified to class based view

--- app/views.py
+++ app/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 from .models import Post
 
 # Create your views here.
+
+
+class PostListView(ListView):
+    """
+    Altennative Post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_list(request):
