d5d8e06b1e685e661f43a71f547206acb92c4039
Yerassyl <erasik01@gmail.com>
2024-05-23 17:07:06 +0500

    comment system added

--- app/views.py
+++ app/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
 from django.views.generic import ListView
-from .forms import EmailPostForm
-from .models import Post
+from django.views.decorators.http import require_POST
+from .forms import EmailPostForm, CommentForm
+from .models import Post, Comment
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -44,14 +45,14 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day)
     
-    # try:
-    #     post = Post.published.get(id=id)
-    # except Post.DoesNotExist:
-    #     raise Http404('No post found.')
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
     
     return render(request, 
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   'comments': comments,
+                   'form': form})
 
 
 def post_share(request, post_id):
@@ -77,3 +78,18 @@ def post_share(request, post_id):
     return render(request, 'blog/post/share.html', {'post': post,
                                                     'form': form,
                                                     'sent': sent})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, 'blog/post/comment.html',
+                  {'post': post,
+                   'form': form,
+                   'comment': comment})
