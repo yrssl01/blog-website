480e14fa2f68b09e5775d6c8c5592611cbbc92c2
Yerassyl <erasik01@gmail.com>
2024-05-29 15:52:13 +0500

    search modified

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ import os
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from django.db.models import Count
 from django.views.generic import ListView
 from django.views.decorators.http import require_POST
@@ -116,9 +116,12 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
             results = Post.published.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')
 
     return render(request,
                   'blog/post/search.html',
