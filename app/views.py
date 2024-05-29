a6794aedfeac0adf1fadc38d5b3cf35561ef9b28
Yerassyl <erasik01@gmail.com>
2024-05-29 15:17:21 +0500

    added post searching

--- app/views.py
+++ app/views.py
@@ -2,10 +2,11 @@ import os
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
+from django.contrib.postgres.search import SearchVector
 from django.db.models import Count
 from django.views.generic import ListView
 from django.views.decorators.http import require_POST
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from .models import Post, Comment
 from taggit.models import Tag
 from dotenv import load_dotenv
@@ -104,3 +105,23 @@ def post_comment(request, post_id):
                   {'post': post,
                    'form': form,
                    'comment': comment})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                search=SearchVector('title', 'body'),
+            ).filter(search=query)
+
+    return render(request,
+                  'blog/post/search.html',
+                  {'form': form,
+                   'query': query,
+                   'results': results})
