df1d776a1e8806aa16df25c263d576a9693e5bbf
Yerassyl <erasik01@gmail.com>
2024-05-29 12:27:55 +0500

    similar posts added, [html] updated

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ import os
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
+from django.db.models import Count
 from django.views.generic import ListView
 from django.views.decorators.http import require_POST
 from .forms import EmailPostForm, CommentForm
@@ -53,12 +54,16 @@ def post_detail(request, year, month, day, post):
     
     comments = post.comments.filter(active=True)
     form = CommentForm()
-    
+
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]   
     return render(request, 
                   'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
-                   'form': form})
+                   'form': form,
+                   'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
